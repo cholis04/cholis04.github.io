58457c43503552db6c2959984fbf0d19f9c216e9
Nurcholis <64176670+cholis04@users.noreply.github.com>
2020-10-18 08:23:39 +0700

    Upload Main Files

---
+++ script.js
@@ -0,0 +1,68 @@
+	/* BACK LISTENER */
+	
+	let back = document.getElementsByClassName('back-button');
+	for(let x = 0; x < back.length; x++ ){
+		back[x].addEventListener('click',function(){		
+			let leftSide = document.getElementById('left');
+			leftSide.style.marginLeft = '-120%';
+		});
+	}
+	
+	/* LINK LISTENER */
+	let links = document.getElementsByClassName('linked');
+	for(let i = 0; i < links.length; i++ ){
+		links[i].addEventListener('click',function(e){
+		
+			
+			let leftSide = document.getElementById('left');
+			let contents = document.getElementsByClassName('content');
+			
+			for(let j = 0; j < contents.length; j++ ){
+				contents[j].style.setProperty('display','none');
+			}
+		
+			let content_active = links[i].dataset.id;
+			let active = document.getElementById(content_active);
+			
+			active.style.setProperty('display','block');
+			
+			leftSide.style.marginLeft = '0';
+			
+			
+			
+		});
+	}
+	
+	
+	window.addEventListener('resize',function(){
+		let w = window.innerWidth;
+		let h = window.innerHeight;
+		
+		let leftSide = document.getElementById('left');
+		
+		if(w>960){
+			leftSide.style.marginLeft = '0%';
+		}else{
+			leftSide.style.marginLeft = '-120%';
+		}
+		
+	});
+	
+	/*
+	function setMarginLeft(x){
+	
+		let leftSide = document.getElementById('left');
+		
+		if(x.matches){
+			leftSide.style.marginLeft = '-120%';
+		}else {
+			leftSide.style.marginLeft = '0';
+		}
+	}
+	
+	let x = window.matchMedia("(max-width:480px)");
+	setMarginLeft(x);
+	x.addListener(setMarginLeft);
+	*/
+
+	
